d6d20ab7572c2b1a2111fc478c647a5027a680fd
Miquel Bosch <miquel.bosch@thetrainline.com>
2025-12-29 13:17:21 +0100

    replace loadcommand completion to return a result type rather than an optional error

--- EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCase.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCase.swift
@@ -31,8 +31,14 @@ final class LoadFeedFromCacheUseCase: XCTestCase {
         let exp = expectation(description: "Wait for load completion")
         
         var receivedError: Error?
-        sut.load { error in
-            receivedError = error
+        sut.load { result in
+            switch result {
+            case let .failure(error):
+                receivedError = error
+            default:
+                XCTFail("Expected failure got result instead.")
+            }
+            
             exp.fulfill()
         }
         
@@ -40,6 +46,22 @@ final class LoadFeedFromCacheUseCase: XCTestCase {
         wait(for: [exp], timeout: 1)
         XCTAssertEqual(receivedError as NSError?, retrievalError)
     }
+    
+//    func test_load_deliversNoImagesOnEmptyCache() {
+//        let (sut, store) = makeSUT()
+//        let retrievalError = anyNSError()
+//        let exp = expectation(description: "Wait for load completion")
+//        
+//        var receivedImages: [FeedImage]?
+//        sut.load { result in
+//            
+//            exp.fulfill()
+//        }
+//        
+//        store.completeRetrieval(with: retrievalError)
+//        wait(for: [exp], timeout: 1)
+//        XCTAssertEqual(receivedError as NSError?, retrievalError)
+//    }
     
     // MARK: - Helpers
     
