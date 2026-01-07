b6afeb2a8fa79f0e1135909889d4daf58c6d640c
Miquel Bosch <miquel.bosch@thetrainline.com>
2026-01-07 12:52:59 +0100

    load command deletes 7 days old caches upon retreival

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -45,7 +45,11 @@ public final class LocalFeedLoader {
                 
             case let .found(feed, timestamp) where self.validate(timestamp):
                 completion(.success(feed.toModels()))
-            case .found, .empty:
+                
+            case .found: // found but not valid
+                self.store.deleteCachedFeed { _ in }
+                completion(.success([]))
+            case .empty:
                 completion(.success([]))
             }
         }
