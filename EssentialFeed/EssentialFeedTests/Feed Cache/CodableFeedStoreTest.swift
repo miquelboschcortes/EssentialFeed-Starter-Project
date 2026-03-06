af08af4a0a3d425534e3cee20a1e6c550dbca1cb
Miquel Bosch <miquel.bosch@thetrainline.com>
2026-03-06 18:20:17 +0100

    Extract FailableInsertFeedStoreSpecs checks into reusable assertions

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTest.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTest.swift
@@ -89,12 +89,9 @@ class CodableFeedStoreTest: XCTestCase, FailableFeedStoreSpecs {
     func test_insert_deliversErrorOnInsertionError() {
         let invalidStoreURL = URL(string: "invalid-url")!
         let sut = makeSUT(storeURL: invalidStoreURL)
+
         
-        let feed = uniqueImageFeed().local
-        let timestamp = Date()
-        
-        let error = insert((feed, timestamp), to: sut)
-        XCTAssertNotNil(error, "Expected to insert and get an error")
+        assertThatInsertDeliversErrorOnInsertionError(on: sut)
     }
     
     func test_insert_overridesPreviouslyInsertedCacheValues() {
@@ -103,18 +100,14 @@ class CodableFeedStoreTest: XCTestCase, FailableFeedStoreSpecs {
         assertThatInsertOverridesPreviouslyInsertedCacheValues(on: sut)
     }
     
-    // MARK: - Delete
-    
     func test_insert_hasNoSideEffectsOnInsertionError() {
         let invalidStoreURL = URL(string: "invalid://store-url")!
         let sut = makeSUT(storeURL: invalidStoreURL)
-        let feed = uniqueImageFeed().local
-        let timestamp = Date()
 
-        insert((feed, timestamp), to: sut)
-
-        expect(sut, toRetrieve: .empty)
+        assertThatInsertHasNoSideEffectsOnInsertionError(on: sut)
     }
+    
+    // MARK: - Delete
     
     func test_delete_deliversNoErrorOnEmptyCache() {
         let sut = makeSUT()
